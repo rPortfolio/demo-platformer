555983f3f3afc78a161298d46321ee3aaad5f6ce
Romeo Cifuentes-Beach <rcifuentesbeach@ucvts.org>
2024-01-24 23:41:49 -0500

    Cleaned Code

--- player/player.gd
+++ player/player.gd
@@ -14,17 +14,17 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
 	if is_on_wall() and velocity.y > 0 and Input.get_axis("ui_left", "ui_right") != 0:
 		velocity.y = 16
-
+	
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or is_on_wall()):
 		velocity.y = jump_velocity
-
+	
 	if is_on_ceiling() and is_on_floor():
 		player_crushed.emit()
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -32,5 +32,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
